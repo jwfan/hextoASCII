ea372bdd58282acca72f5a22e9132f0c3b044fac
jasonfisherjlf <jason.fisher.jlf@gmail.com>
2017-10-18 08:45:46 -0600

    Update hex_to_ascii.py
    I'm not entirely sure why you had some code that you did. I cleaned up what made sense to me, and made the code account for, and handle newlines. This is particularly a problem if you are working with hex values from a hexdump, or if sublime is convinced that the file you are working on is a binary one. Essentially, the values were presented as such:
    xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
    xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
    ...
    
    This would throw an error, but the this proposed change will handle it.

--- hex_to_ascii.py
+++ hex_to_ascii.py
@@ -11,24 +11,17 @@ class HexToAsciiCommand(sublime_plugin.TextCommand):
         for j in range(0, len(reglist)):
             hx = v.substr(v.sel()[j])
             hx = hx.strip('{} .,')
-            if ',' in hx:
-                hx = hx.replace(', ', ' ')
-                hx = hx.replace(',', ' ')
-            if '0x' in hx:
-                hx = hx.replace('0x', '')
+            hx = hx.replace(' ', '')
+            hx = hx.replace(',', '')
+            hx = hx.replace('0x', '')
+            hx = hx.replace('\n', '')
             astr = ''
-            if ' ' in hx:
-                t = 1
-            else:
-                t = 2
-            if (len(hx) % 2 != 0) and (t == 2):
+            if (len(hx) % 2 != 0):
                 sublime.status_message("Perhaps we lost key \"%s\" in the end of line?" % hx[len(hx) - 1])
-            for i in range(0, len(hx)-1, t):
+            for i in range(0, len(hx)-1, 2):
                 if not(hx[i] in hexdig) or not(hx[i+1] in hexdig):
                     sublime.error_message("\"%s\" isn't a part of hexadecimal number!" % hx[i:i+2])
                     break
-                if (hx[i] == ' ') or (hx[i+1] == ' '):
-                    continue
                 st = hx[i]+hx[i+1]
                 astr = astr + chr(int(st, 16))
                 v.replace(edit, v.sel()[j], astr)
